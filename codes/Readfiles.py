cc988eac5ea2309952e783c809f6be5b1b96c241
Sungkeun Lim <limsungkeunlee@gmail.com>
2017-04-17 12:45:17 +0900

    thanks..

--- codes/Readfiles.py
+++ codes/Readfiles.py
@@ -5,7 +5,9 @@ def getFnames(dir, dtype="apr", minimumsize=7000.):
 # def getFnames(dir, type="apr", minimumsize=7000.):
 
     fnames = []
-    os.chdir("../data/ChungCheonDC/")
+#    os.chdir("../data/ChungCheonDC/")
+    os.chdir("../data/IdongDC/")
+
     # print glob.glob("*.apr")
     for file in glob.glob("*.apr"):
         if os.path.getsize(file) > minimumsize:
